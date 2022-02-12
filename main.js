931d14b2529cc1092799cf65aabac1298ff11783
Pavani260410 <69670894+Pavani260410@users.noreply.github.com>
2022-02-12 11:30:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/94XqLdWvh/model.json", modelReady);
+
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+    console.log(gotResuts);
+}
